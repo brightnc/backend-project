e8b3200a5395a3ee46f6fe1c7069520967032310
Siwat Anandechsak <bright.siwat@hotmail.com>
2023-11-01 15:47:55 +0700

    createUser Repository

--- src/repositories/user.ts
+++ src/repositories/user.ts
@@ -1,9 +1,21 @@
 import { PrismaClient } from "@prisma/client";
-import { IUserRepository } from ".";
+import { IUser, IUserRepository } from ".";
+import { ICreateUserDTO, IUserDTO } from "../dto/user";
 
 export default class UserRepository implements IUserRepository {
   private prisma: PrismaClient;
   constructor(prisma: PrismaClient) {
     this.prisma = prisma;
+  }
+  createUser(user: ICreateUserDTO): Promise<IUser> {
+    return this.prisma.user.create({
+      data: user,
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        registeredAt: true,
+      },
+    });
   }
 }
