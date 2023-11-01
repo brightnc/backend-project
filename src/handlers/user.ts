ab5c5b7575fc880d96d6a1fbb24ae7c365fa8fd9
Siwat Anandechsak <bright.siwat@hotmail.com>
2023-11-01 16:38:19 +0700

    createUser Handler

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,9 +1,33 @@
 import { IUserHandler } from ".";
+import { IUserDTO } from "../dto/user";
 import { IUserRepository } from "../repositories";
+import { hashPassword } from "../utils/bcrypt";
 
 export default class UserHandler implements IUserHandler {
   private repo: IUserRepository;
   constructor(repo: IUserRepository) {
     this.repo = repo;
   }
+
+  registration: IUserHandler["registration"] = async (req, res) => {
+    const { name, username, password } = req.body;
+    try {
+      const result = await this.repo.createUser({
+        name,
+        username,
+        password: hashPassword(password),
+      });
+
+      const userResponse: IUserDTO = {
+        ...result,
+        registeredAt: result.registeredAt.toUTCString(),
+      };
+
+      return res.status(201).json(userResponse).end();
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ message: error.message }).end();
+      }
+    }
+  };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import { IUserRepository } from "./repositories";
+import UserRepository from "./repositories/user";
+import { IUserHandler } from "./handlers";
+import UserHandler from "./handlers/user";
 
 const app = express();
 const PORT = Number(process.env.PORT || 8800);
 
 const client = new PrismaClient();
+const userRepo: IUserRepository = new UserRepository(client);
+const userHandler: IUserHandler = new UserHandler(userRepo);
+
+app.use(express.json());
+const userRouter = express.Router();
 
 app.get("/", (req, res) => {
   return res.status(200).send("Hello world").end();
 });
+
+app.use("/user", userRouter);
+
+userRouter.post("/", userHandler.registration);
 
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
