5ac3cf6c80501ea040de217c78338e0607e5dc9f
Siwat Anandechsak <bright.siwat@hotmail.com>
2023-11-03 11:17:19 +0700

    get data from oEmbed api

---
+++ src/dto/oembed.ts
@@ -0,0 +1,24 @@
+export interface OEmbedResponseDto {
+  url: string;
+  error: string;
+  thumbnail_height: number;
+  version: string;
+  width: number;
+  thumbnail_width: number;
+  provider_name: string;
+  thumbnail_url: string;
+  author_url: string;
+  author_name: string;
+  title: string;
+  type: string;
+  height: number;
+  provider_url: string;
+  html: string;
+}
+
+export interface OEmbedData {
+  title: string;
+  thumbnail_url: string;
+  author_name: string;
+  author_url: string;
+}
